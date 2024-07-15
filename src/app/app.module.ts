c81a63cecd9f88849a713b6bf2a051de0f95d853
Santosh kumar Singh <santoshfrombihar8@gmail.com>
2024-07-15 10:53:38 +0530

    Teacher component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { SellerComponent } from './seller/seller.component';
+import { SellerComponent } from './Teachers/seller/seller.component';
 import { HomeComponent } from './home/home.component';
 import { FormsModule } from '@angular/forms';
+import { WelcomepageComponent } from './Teachers/welcomepage/welcomepage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     SellerComponent,
-    HomeComponent
+    HomeComponent,
+    WelcomepageComponent
   ],
   imports: [
     BrowserModule,
